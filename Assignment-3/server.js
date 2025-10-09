fa768f374961bdaa43fa099988b98583ffd51c05
Yash Patel <patelyashu001@gmail.com>
2025-10-09 22:53:26 +0530

    server.js

--- Assignment-3/server.js
+++ Assignment-3/server.js
@@ -32,5 +32,4 @@ app.use("/api/posts", require("./routes/postRoutes"));
 
 // ✅ Start server
 const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-module.exports = app;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
