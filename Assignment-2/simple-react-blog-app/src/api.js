dd084c0c5847ab1762ce78d6f4b509c00c3787e1
Yash Patel <patelyashu001@gmail.com>
2025-10-09 22:45:56 +0530

    Backend Url

--- Assignment-2/simple-react-blog-app/src/api.js
+++ Assignment-2/simple-react-blog-app/src/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "blog-backend-zeta-blond.vercel.app/api",
+  baseURL: "https://blog-backend-zeta-blond.vercel.app/api",
 });
 
 api.interceptors.request.use((config) => {
